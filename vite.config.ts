1956ff06419c84c1a7f093bc7b8b6854b655315f
Van den Berghe Jo <jo.van.den.berghe@gmail.com>
2025-07-25 00:07:57 +0000

    Set base URL for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: process.env.CI ? '/camera-slider-test/' : '/',
   server: {
     port: 3000,
   },
